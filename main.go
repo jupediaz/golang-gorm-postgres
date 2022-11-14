7869f0bc661f1d7f96da2da7e05b7f268bb50e5d
JJDiaz <jose@jjdiaz.dev>
2022-11-14 13:12:27 +0100

    Healthchecker update

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
-		message := "Welcome to Golang with Gorm and Postgres"
+		message := "service available"
 		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
 	})
 
